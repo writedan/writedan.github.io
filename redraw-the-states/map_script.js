4795753c0a4febc87a073ced02a686554f463e17
BuildTools <unconfigured@null.spigotmc.org>
2019-10-25 15:36:31 -0400

    Separated functions into separate files

---
+++ redraw-the-states/map_script.js
@@ -0,0 +1,19 @@
+function draw_map(features) {
+  var width = 960,
+      height = 500;
+
+  var path = d3.geo.path();
+
+  var svg = d3.select("body").append("svg")
+      .attr("width", width)
+      .attr("height", height);
+
+    svg.selectAll("path")
+        .data(features)
+      .enter().append("path")
+        .attr("d", path)
+  	    .attr("id",function(d){
+  		      return "county" + d.properties.GEOID;
+  	    });
+  });
+}
